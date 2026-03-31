a57dc237f66f5c870740e73a1fe66695741247a7
kuberbassi <kuberbassi2007@gmail.com>
2026-03-31 21:23:52 +0530

    initial

---
+++ prime.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int n,i,isPrime=1;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    if (n<=1){
+        printf("Not a prime number.");
+        return 0;
+    }
+    for( i = 2; i<n ; i++){
+        if(n%i == 0){
+            isPrime= 0;
+            break;
+        }
+    }
+    if (isPrime){
+        printf("%d is a prime number.", n);}
+    else{
+        printf("Not a prime number.");}
+
+    return 0;
+}
